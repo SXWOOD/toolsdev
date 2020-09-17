7d985a6da4b9a0b1e0cc54bb504580fffd2ce966
SXWOOD <71347500+SXWOOD@users.noreply.github.com>
2020-09-17 18:28:43 -0500

    Scenefiles Fixed
    Fixed scenefiles to have one match with every video

--- scenefile_v2.py
+++ scenefile_v2.py
@@ -1,11 +1,11 @@
-# {folder_path}/{descriptor}_{task}_{v###}.{ext}
-from pathlib import Path
-
-
 class SceneFile(object):
     """"An abstract representation of a Scene file."""
-    def __init__(self, path):
-        self._init_from_path(path)
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
 
     @property
     def filename(self):
@@ -15,18 +15,6 @@ class SceneFile(object):
                               ver=self.ver,
                               ext=self.ext)
 
-    @property
-    def path(self):
-        return self.folder_path / self.filename
 
-    def _init_from_path(self, path):
-        path = Path(path)
-        self.folder_path = path.parent
-        self.ext = path.suffix
-        self.descriptor, self.task, ver = path.stem.split("_")
-        self.ver = int(ver.split("v")[-1])
-
-
-scene_file = SceneFile("D:/tank_model_v001.ma")
-print(scene_file.path)
+scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
 print(scene_file.filename)
